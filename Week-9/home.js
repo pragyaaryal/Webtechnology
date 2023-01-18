06dc67c13328d870e5ded43b3ab7307d67cc67ed
pragyaryal <np03cs4a220022@heraldcollege.edu.np>
2023-01-18 14:06:31 +0545

    Webtechnology

---
+++ Week-9/home.js
@@ -0,0 +1,19 @@
+// console.log("Hello i dont like Binay");
+
+/*let a = 5;
+let b = 3;
+let sum = a + b;
+console.log(sum);
+
+let a = 5;
+let b = 3;
+let sum = a + b;
+alert(sum);
+
+console.log(typeof(a));
+
+let radius = window.prompt("Enter a radius value:");
+const PI = 3.14;
+let Volume = 4/3 * PI * radius * radius * radius;
+console.log(Volume);*/
+
