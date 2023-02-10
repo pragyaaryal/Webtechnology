0b6cd2c74f636bcb6f7038f16e066de22d1d1087
pragyaryal <np03cs4a220022@heraldcollege.edu.np>
2023-02-10 20:50:04 +0545

    Portfolio

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,16 +1,43 @@
-document.addEventListener('scroll', timeline);
+const form = document.querySelector('form');
+const firstname = document.getElementById('fname');
+const lastname = document.getElementById('lname');
+const email = document.getElementById('email');
+const province = document.getElementById('province');
+const subject = document.getElementById('subject');
 
-function timeline(){
-	var threshold_position = window.scrollY + window.innerHeight * 2/3;
-	//compare scrolltop with scrolltop on each timeline event
-	var timeline_events = document.querySelectorAll('.timeline li');
-	for(i in timeline_events){
-		if(timeline_events[i].offsetTop < threshold_position){
-			timeline_events[i].classList.add('visible');
-		} else {
-			timeline_events[i].classList.remove('visible');
-		}
-	}
-}
-timeline();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
+  if (!firstname.value || !lastname.value || !email.value || !province.value || !subject.value) {
+    alert('All fields are required');
+    return;
+  }
+
+  const data = {
+    firstname: firstname.value,
+    lastname: lastname.value,
+    email: email.value,
+    province: province.value,
+    subject: subject.value
+  };
+
+  fetch('https://example.com/send-email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      alert('Your email was sent successfully');
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        alert('An error has occurred while sending the email, please try again later or contact the administrator.');
+      });
